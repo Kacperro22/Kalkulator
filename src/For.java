5d49a904e7781dd7c0d2a36ea3395581eb89b34c
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-23 20:19:48 +0100

    Do while test

--- src/For.java
+++ src/For.java
@@ -1,11 +1,24 @@
 public class For {
     public static void main(String[] args) {
 
-        String[] cats = new String[]{"Tobi","Daisy","Tofi","Jania"};
+//        String[] cats = new String[]{"Tobi","Daisy","Tofi","Jania"};
+//
+//        for(int i=0; i<cats.length; i++){
+//            System.out.println(cats[i]);
+//        }
 
-        for(int i=0; i<cats.length; i++){
-            System.out.println(cats[i]);
-        }
+        int x=3;
+        int y;
+        System.out.println(x++); //3
+        //4
+        System.out.println(++x); //5
+        System.out.println(x);//5
+        y = x++; //5
+        System.out.println (y); //5
+        y = ++x; //7
+        System.out.println (y); //7
+        System.out.println (++y); //8
+
 
 
 
