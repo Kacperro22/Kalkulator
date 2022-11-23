b3773f3d08d74274bbbfeeac2bb21956b55dfe8f
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-23 19:49:23 +0100

    Do while

--- src/While.java
+++ src/While.java
@@ -1,13 +1,12 @@
-public class DoWhile {
+public class While {
     public static void main(String[] args) {
-
         int counter = 0;
 
-        do{
+        while(counter <=10){
             System.out.println(counter);
             counter++;
-        }while(counter <= 10) ;
 
 
+        }
     }
 }
