47cf443f629f3eb8da1ff7926801fb6b5287e775
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-23 19:26:47 +0100

    for each

---
+++ src/ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach {
+    public static void main(String[] args) {
+
+        String[] cats = new String[]{"Tobi","Daisy","Tofi","Jania"};
+
+        for (String cat:cats) {
+            System.out.println(cat);
+        }
+    }
+}
