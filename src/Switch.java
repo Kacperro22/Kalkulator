c2aa38e64fe02adb80aef9cae649ab94a384e40b
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-23 20:30:52 +0100

    switch

--- src/Switch.java
+++ src/Switch.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Switch {
     public static void main(String[] args) {
-        switch
+        Scanner skaner = new Scanner(System.in);
+
+        System.out.println("Podaj liczbe: ");
+        int inputNumber = skaner.nextInt();
+
+        switch(inputNumber){
+            case 1:
+                System.out.println("case1");
+                break;
+            case 2:
+                System.out.println("case2");
+                break;
+            case 3:
+                System.out.println("case3");
+                break;
+        }
     }
 }
